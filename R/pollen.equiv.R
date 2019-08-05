6519261bd0a4313045ae44cf33b33d5fb2cd894c
Simon <simon.j.goring@gmail.com>
2019-08-05 00:41:57 -0700

    Putting the pollen equivalency table into `data`.

--- R/pollen.equiv.R
+++ R/pollen.equiv.R
@@ -4,7 +4,7 @@
 #' @title A table to convert the pollen taxa identified by investigators to standardized lists.
 #' @description A list of standardized (published) taxonomies from the literature to help standardize taxonomies for synthesis work.
 #' @docType data
-#' @usage translate.table
+#' @usage data(pollen.equiv)
 #' @format a \code{data.frame} object
 #' @author Simon J. Goring \email{simon.j.goring@@gmail.com}; Jeremiah Marsicek 
 NULL
